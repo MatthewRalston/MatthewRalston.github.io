c9f843742295e762065a7f95e04aa9704b6cfe00
Matthew Ralston <mralston.development@gmail.com>
2025-03-29 00:32:51 -0400

    Revisions to anti-intellectualism blog post.

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1743206731',resources: [
+    cacheName: 'chirpy-1743223539',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
