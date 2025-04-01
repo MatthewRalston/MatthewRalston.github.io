262d3396dca9d865ac0f291b92a52f9c8606f5ea
Matthew Ralston <mralston.development@gmail.com>
2025-04-01 16:52:28 -0400

    Update to about header.

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1743529645',resources: [
+    cacheName: 'chirpy-1743539927',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
