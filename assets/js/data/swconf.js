62ea74b96a23732dda5d3003253053e5667893f8
Matthew Ralston <mralston.development@gmail.com>
2025-04-01 13:49:32 -0400

    Cell image and ngsci.

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1743363131',resources: [
+    cacheName: 'chirpy-1743529645',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
