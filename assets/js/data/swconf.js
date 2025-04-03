5e8534e03ed4f1c8b358e2f4f3b9d55b72070f94
Matthew Ralston <mralston.development@gmail.com>
2025-04-02 20:58:08 -0400

    Some updates to civil rights post. Working on civil rights act history during civil rights movement 1950s - 1970s.

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1743553494',resources: [
+    cacheName: 'chirpy-1743641849',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
