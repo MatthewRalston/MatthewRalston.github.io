0719d9d6d8df1ea531a0d42a6e4efbc20d839abe
Matthew Ralston <mralston.development@gmail.com>
2025-04-05 10:55:14 -0400

    Update to the odds ratio, risk ratio, and calculations. Included other ethnicities. These are neither 'bad' nor 'good' numbers, and definitely not 'ideal' numbers, to begin with. I am not minimizing at all their significance on discourse, but i did just show the numbers, so this paints my bias, which isnt the same thing as being a good person. Just that numbers are numbers, you should trust numbers given, only those you understand how to arrive at yourself, and understand the recipe for those numbers, not just repeating the calculations. Go get the data, learn the wikipedia articles, understand and practice, and know the difference between a 'good' number like 2x vs an 'absurd number' like 0.00000001, or 2050%... LMAO. Okay, its not about taking money from people. Its about courtesy and sharing amongst yaselves and your neighbors. Always. Thats an unbeatable lesson. Speaking of beatings, Elon and his proud boy compadres could do with some tping and some like... tagging their houses right about now, but dont necessarily hurt or expose people. Thats illegal. God and Goddess will get theirs. Dont take justice in your own hands over something stupid like numbers. Work on fixing them! Not on anger!

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1743714018',resources: [
+    cacheName: 'chirpy-1743864600',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
