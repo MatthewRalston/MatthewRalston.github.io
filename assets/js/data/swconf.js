084a89b8029ab9f3a9b9230c45db683712dfe6e3
Matthew Ralston <mralston.development@gmail.com>
2025-04-19 17:10:22 -0400

    Update to blog post on social media and AI generated content/dead internet theory.

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1743997287',resources: [
+    cacheName: 'chirpy-1745096996',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
