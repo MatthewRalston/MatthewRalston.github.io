8505c2ad0d5b0135c58efb95f6ee7a7f8d4613ca
Matthew Ralston <mralston.development@gmail.com>
2025-04-20 13:47:25 -0400

    Meme update to basic python 2.

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1745096996',resources: [
+    cacheName: 'chirpy-1745196393',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
