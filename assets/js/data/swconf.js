dbdcf0ab68a1fe3bf0eb3c4f4b8535a84b059949
Matthew Ralston <mralston.development@gmail.com>
2025-05-26 07:41:57 -0400

    Updates CV files and /cv page.

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1745513770',resources: [
+    cacheName: 'chirpy-1748259682',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
